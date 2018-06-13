2a4aa47e6db734702b3b9c7a082973fe96497781
matthewsyren <matthew.syren@gmail.com>
2018-06-13 08:30:19 +0200

    Added a Widget layout for when the user is signed out

--- Runner/app/src/main/java/com/matthewsyren/runner/widget/WeeklyGoalsWidget.java
+++ Runner/app/src/main/java/com/matthewsyren/runner/widget/WeeklyGoalsWidget.java
@@ -9,8 +9,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
+import android.view.View;
 import android.widget.RemoteViews;
 
+import com.matthewsyren.runner.MainActivity;
 import com.matthewsyren.runner.R;
 import com.matthewsyren.runner.WeeklyGoalsActivity;
 import com.matthewsyren.runner.models.Run;
@@ -27,49 +29,68 @@ import java.util.ArrayList;
  */
 public class WeeklyGoalsWidget
         extends AppWidgetProvider {
-    ArrayList<Run> mRuns;
-    Target mTarget;
-    Context mContext;
+    private ArrayList<Run> mRuns;
+    private Target mTarget;
+    private Context mContext;
+    private boolean mIsUserSignedIn;
 
     void updateAppWidget(final Context context, final AppWidgetManager appWidgetManager, final int appWidgetId) {
         //Creates the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weekly_goals_widget);
+        RemoteViews views;
 
-        //Initialises the total variables
-        double totalDistance = 0;
-        int totalDuration = 0;
+        //Displays the appropriate Widget layout based on whether the user is signed in or not
+        if(mIsUserSignedIn) {
+            //Fetches the Widget layout that displays the user's progress towards their targets
+            views = new RemoteViews(context.getPackageName(), R.layout.weekly_goals_widget);
 
-        //Calculates the totals
-        for(Run run : mRuns){
-            totalDistance += run.getDistanceTravelled();
-            totalDuration += run.getRunDuration();
+            //Initialises the total variables
+            double totalDistance = 0;
+            int totalDuration = 0;
+
+            //Calculates the totals
+            for(Run run : mRuns){
+                totalDistance += run.getDistanceTravelled();
+                totalDuration += run.getRunDuration();
+            }
+
+            //Calculates the user's average speed
+            int averageSpeed = RunInformationFormatUtilities.getUsersAverageSpeedInKilometresPerHour(totalDistance, totalDuration);
+
+            //Converts distance to kilometres
+            totalDistance /= 1000;
+
+            //Displays the user's distance progress
+            int distanceProgress = WeeklyGoalsUtilities.getDistanceProgress(totalDistance, mTarget.getDistanceTarget());
+            views.setProgressBar(R.id.pb_widget_distance_target, 100, distanceProgress, false);
+            views.setTextViewText(R.id.tv_widget_distance_target, context.getString(R.string.distance_target_progress, totalDistance, mTarget.getDistanceTarget()));
+
+            //Displays the user's duration progress
+            int durationProgress = WeeklyGoalsUtilities.getDurationProgress(totalDuration, mTarget.getDurationTarget());
+            views.setProgressBar(R.id.pb_widget_duration_target, 100, durationProgress, false);
+            views.setTextViewText(R.id.tv_widget_duration_target, context.getString(R.string.duration_target_progress, totalDuration, mTarget.getDurationTarget()));
+
+            //Displays the user's duration progress
+            int averageSpeedProgress = WeeklyGoalsUtilities.getAverageSpeedProgress(averageSpeed, mTarget.getAverageSpeedTarget());
+            views.setProgressBar(R.id.pb_widget_average_speed_target, 100, averageSpeedProgress, false);
+            views.setTextViewText(R.id.tv_widget_average_speed_target, context.getString(R.string.average_speed_target_progress, averageSpeed, mTarget.getAverageSpeedTarget()));
+
+            //Creates a PendingIntent that will open WeeklyGoalsActivity when the user clicks on the Widget heading
+            Intent intent = new Intent(context, WeeklyGoalsActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.tv_widget_heading, pendingIntent);
+
+            //Displays the Widget when the user is signed in
+            views.setViewVisibility(R.id.ll_widget_weekly_goals, View.VISIBLE);
         }
+        else{
+            //Fetches the Widget layout that displays a signed out message
+            views = new RemoteViews(context.getPackageName(), R.layout.weekly_goals_widget_signed_out);
 
-        //Calculates the user's average speed
-        int averageSpeed = RunInformationFormatUtilities.getUsersAverageSpeedInKilometresPerHour(totalDistance, totalDuration);
-
-        //Converts distance to kilometres
-        totalDistance /= 1000;
-
-        //Displays the user's distance progress
-        int distanceProgress = WeeklyGoalsUtilities.getDistanceProgress(totalDistance, mTarget.getDistanceTarget());
-        views.setProgressBar(R.id.pb_widget_distance_target, 100, distanceProgress, false);
-        views.setTextViewText(R.id.tv_widget_distance_target, context.getString(R.string.distance_target_progress, totalDistance, mTarget.getDistanceTarget()));
-
-        //Displays the user's duration progress
-        int durationProgress = WeeklyGoalsUtilities.getDurationProgress(totalDuration, mTarget.getDurationTarget());
-        views.setProgressBar(R.id.pb_widget_duration_target, 100, durationProgress, false);
-        views.setTextViewText(R.id.tv_widget_duration_target, context.getString(R.string.duration_target_progress, totalDuration, mTarget.getDurationTarget()));
-
-        //Displays the user's duration progress
-        int averageSpeedProgress = WeeklyGoalsUtilities.getAverageSpeedProgress(averageSpeed, mTarget.getAverageSpeedTarget());
-        views.setProgressBar(R.id.pb_widget_average_speed_target, 100, averageSpeedProgress, false);
-        views.setTextViewText(R.id.tv_widget_average_speed_target, context.getString(R.string.average_speed_target_progress, averageSpeed, mTarget.getAverageSpeedTarget()));
-
-        //Creates a PendingIntent that will open WeeklyGoalsActivity when the user clicks on the Widget heading
-        Intent intent = new Intent(context, WeeklyGoalsActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.tv_widget_heading, pendingIntent);
+            //Creates a PendingIntent that will open MainActivity when the user clicks on the Widget heading
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            views.setOnClickPendingIntent(R.id.tv_widget_heading, pendingIntent);
+        }
 
         //Displays the updated values in the Widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -79,8 +100,22 @@ public class WeeklyGoalsWidget
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         mContext = context;
 
-        //Requests the user's targets and runs
-        new Target().requestTargetsAndRuns(context, PreferenceUtilities.getUserKey(context), new DataReceiver(new Handler()));
+        //Fetches the user's unique key
+        String userKey = PreferenceUtilities.getUserKey(context);
+
+        //Determines whether the user is signed in or not based on if their key has a value
+        if(userKey != null){
+            mIsUserSignedIn = true;
+
+            //Requests the user's targets and runs, as the user is signed in
+            new Target().requestTargetsAndRuns(context, userKey, new DataReceiver(new Handler()));
+        }
+        else{
+            mIsUserSignedIn = false;
+
+            //Will display a Widget that tells the user to sign in
+            updateWidgets();
+        }
     }
 
     @Override
@@ -91,6 +126,18 @@ public class WeeklyGoalsWidget
     @Override
     public void onDisabled(Context context) {
 
+    }
+
+    //Updates all Widgets
+    private void updateWidgets(){
+        //Gets the Widgets to update
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(mContext, WeeklyGoalsWidget.class));
+
+        //Updates the Widgets with the new values
+        for(int appWidgetId : appWidgetIds){
+            updateAppWidget(mContext, appWidgetManager, appWidgetId);
+        }
     }
 
     //Used to retrieve results from the FirebaseService
@@ -111,14 +158,8 @@ public class WeeklyGoalsWidget
                 mRuns = resultData.getParcelableArrayList(FirebaseService.RUNS_EXTRA);
                 mTarget = resultData.getParcelable(FirebaseService.TARGET_EXTRA);
 
-                //Gets the Widgets to update
-                AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
-                int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(mContext, WeeklyGoalsWidget.class));
-
-                //Updates the Widgets with the new values
-                for(int appWidgetId : appWidgetIds){
-                    updateAppWidget(mContext, appWidgetManager, appWidgetId);
-                }
+                //Updates all the Widgtes
+                updateWidgets();
             }
         }
     }

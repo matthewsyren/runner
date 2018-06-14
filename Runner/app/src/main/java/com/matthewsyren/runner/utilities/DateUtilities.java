cff75c8871703fafd398a28bd568a297f4228920
matthewsyren <matthew.syren@gmail.com>
2018-06-14 08:59:09 +0200

    Added a method to DateUtilities to return all dates from the previous week

--- Runner/app/src/main/java/com/matthewsyren/runner/utilities/DateUtilities.java
+++ Runner/app/src/main/java/com/matthewsyren/runner/utilities/DateUtilities.java
@@ -16,23 +16,46 @@ public class DateUtilities {
     }
 
     /**
-     * Gets all dates for the current week and returns them as a String array
+     * Returns a String array with all dates for the current week
      */
     public static String[] getDatesForCurrentWeek(){
+        //Sets the date to today
+        Calendar calendar = Calendar.getInstance();
+
+        //Returns the dates for this week
+        return getDatesForSpecificWeek(calendar);
+    }
+
+    /**
+     * Returns a String array containing all dates for the week of the date passed in
+     * @param calendar A Calendar object set to a date in the week that you would like to get the dates for
+     */
+    private static String[] getDatesForSpecificWeek(Calendar calendar){
         String[] dates = new String[7];
 
         //Sets the start of the week to Monday
-        Calendar calendar = Calendar.getInstance();
         while(calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY){
             calendar.add(Calendar.DAY_OF_WEEK, -1);
         }
 
-        //Loops through all dates for the current week and adds them to the array
+        //Loops through all dates for the week and adds them to the array
         for(int i = 0; i < 7; i++) {
             dates[i] = formatDate(calendar.getTime());
             calendar.add(Calendar.DAY_OF_WEEK, 1);
         }
 
         return dates;
+    }
+
+    /**
+     * Returns a String array with all dates for the previous week
+     */
+    public static String[] getDatesForPreviousWeek(){
+        //Sets the date to a week ago
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -7);
+
+        //Returns the dates in the previous week
+        return getDatesForSpecificWeek(calendar);
     }
 }
